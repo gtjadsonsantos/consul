0458820a399808830e481ecb2b4f0047f11e527a
Jadson Santos <jadson44.santos@gmail.com>
2021-05-29 16:46:23 -0300

    docs: Update

--- custom_components/consul/catalog/deregister.py
+++ custom_components/consul/catalog/deregister.py
@@ -1,26 +1,46 @@
 from homeassistant.core import ServiceCall
-
-from consulate import Consul
+from requests import put
+import json
+import logging
+_LOGGER = logging.Logger(__name__)
 
 def deregister(call:ServiceCall) -> bool:
 
-    host = call.data.get("host", "")
+    host = call.data.get("host", None)
+    protocol = call.data.get("protocol", "http")
     port = call.data.get("port", 8500)
-    token = call.data.get("token", "")
+    token = call.data.get("token", None)
+    
     node = call.data.get("node", "")
     datacenter = call.data.get("datacenter", None)
     service_id = call.data.get("service_id", None)
     check_id = call.data.get("check_id", None)
-                    
-    consul = Consul(host=host,token=token,datacenter=datacenter,port=port)
+    ns = call.data.get("ns", None)
+
+    payload = {}
+    headers = {}  
+
+    if datacenter:
+        payload['datacenter'] = datacenter
+    if node:
+        payload['node'] = node
+    if check_id:
+        payload['checkid'] = check_id
+    if service_id:
+        payload['serviceid'] = service_id
+    if ns:
+        payload['ns'] = ns
+    if token:
+        headers['X-Consul-Token'] = token
     
-    return consul.catalog.deregister(
-        node=node,
-        check_id=check_id,
-        datacenter=datacenter,
-        service_id=service_id
-    )
-    
+    try:
+        put("{protocol}://{host}:{port}/v1/catalog/deregister".format(protocol=protocol,host=host,port=port),
+        data=json.dumps(payload),
+        headers=headers)
+        return True
+    except IOError as e :
+        _LOGGER.exception(e)
+        return False
     
 
 
--- custom_components/consul/catalog/register.py
+++ custom_components/consul/catalog/register.py
@@ -1,21 +1,54 @@
 from homeassistant.core import ServiceCall
-from consulate import Consul
+from requests import put
+import json
+import logging
+
+_LOGGER = logging.Logger(__name__)
 
 def register(call: ServiceCall) -> bool:
 
-    host = call.data.get("host", "")
+    host = call.data.get("host", None)
+    protocol = call.data.get("protocol", "http")
     port = call.data.get("port", 8500)
-    token = call.data.get("token", "")
+    token = call.data.get("token", None)
+    
     datacenter = call.data.get("datacenter", None)
-    node = call.data.get("node", "")
+    node_id = call.data.get("id", None )
+    node = call.data.get("node", None)
+    address = call.data.get("address", None)
+    taggedaddresses = call.data.get("taggedaddresses", None)
+    node_meta = call.data.get("node_meta", None)
     service = call.data.get("service", None)
     check = call.data.get("check", None)
 
-    consul = Consul(host=host, token=token, datacenter=datacenter, port=port)
+    payload = {}
+    headers = {}
 
-    return consul.catalog.register(
-        node=node,
-        address=host,
-        datacenter=datacenter,
-        service=service,
-        check=check)
+    if datacenter:
+        payload['datacenter'] = datacenter
+    if node_id:
+        payload['id'] = node_id
+    if node:
+        payload['node'] = node
+    if address:
+        payload['address'] = address
+    if taggedaddresses:
+        payload['taggedaddresses'] = taggedaddresses
+    if node_meta:
+        payload['nodemeta'] = node_meta
+    if service:
+        payload['service'] = service
+    if check:
+        payload['check'] = check
+    if token:
+        headers['X-Consul-Token'] = token
+    try:
+        put("{protocol}://{host}:{port}/v1/catalog/register".format(protocol=protocol,host=host,port=port),
+        data=json.dumps(payload),
+        headers=headers)
+        return True
+    except IOError as e :
+        _LOGGER.exception(e)
+        return False
+
+    
