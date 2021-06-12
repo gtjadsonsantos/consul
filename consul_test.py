22d039547eb0458dbbe82756f02e3a441437b906
Jadson Santos <jadson44.santos@gmail.com>
2021-06-12 10:45:04 -0300

    test: Updated cases test

--- consul_test.py
+++ consul_test.py
@@ -4,7 +4,7 @@ from custom_components import consul
 
 PAYLOAD_CATALOG_REGISTER = {
     "host": "192.168.0.21",
-    "port": 8500,
+    "host_port": 8500,
     "token": "57c5d69a-5f19-469b-0543-12a487eecc66",
     "datacenter": "dc1",
     "node": "jadson",
@@ -37,7 +37,7 @@ PAYLOAD_CATALOG_REGISTER = {
 
 PAYLOAD_CATALOG_DEREGISTER = {
     "host": PAYLOAD_CATALOG_REGISTER["host"],
-    "port": PAYLOAD_CATALOG_REGISTER["port"],
+    "host_port": PAYLOAD_CATALOG_REGISTER["host_port"],
     "token": PAYLOAD_CATALOG_REGISTER["token"],
     "node": PAYLOAD_CATALOG_REGISTER["node"],
     "datacenter": PAYLOAD_CATALOG_REGISTER["datacenter"],
@@ -47,7 +47,7 @@ PAYLOAD_CATALOG_DEREGISTER = {
 
 PAYLOAD_KV_CREATE_OR_UPDATE = {
     "host": PAYLOAD_CATALOG_REGISTER["host"],
-    "port": PAYLOAD_CATALOG_REGISTER["port"],
+    "host_port": PAYLOAD_CATALOG_REGISTER["host_port"],
     "token": PAYLOAD_CATALOG_REGISTER["token"],
     "key": "jadson179",
     "value": "https://github.com/jadson179",
@@ -55,10 +55,41 @@ PAYLOAD_KV_CREATE_OR_UPDATE = {
 
 PAYLOAD_KV_DELETE = {
     "host": PAYLOAD_CATALOG_REGISTER["host"],
-    "port": PAYLOAD_CATALOG_REGISTER["port"],
+    "host_port": PAYLOAD_CATALOG_REGISTER["host_port"],
     "token": PAYLOAD_CATALOG_REGISTER["token"],
     "key": PAYLOAD_KV_CREATE_OR_UPDATE["key"]
 }
+
+PAYLOAD_AGENT_SERVICE_REGISTER = {
+    "host": PAYLOAD_CATALOG_REGISTER["host"],
+    "host_port": PAYLOAD_CATALOG_REGISTER["host_port"],
+    "token": PAYLOAD_CATALOG_REGISTER["token"],
+    "id": "redis1",
+    "name": "redis",
+    "tags": ["primary", "v1"],
+    "address": "127.0.0.1",
+    "port": 8000,
+    "meta": {
+        "redis_version": "4.0"
+    },
+    "enabletagoverride": False,
+    "weights": {
+        "passing": 10,
+        "warning": 1
+    }
+}
+
+PAYLOAD_AGENT_SERVICE_DEREGISTER = {
+    "host": PAYLOAD_CATALOG_REGISTER["host"],
+    "host_port": PAYLOAD_CATALOG_REGISTER["host_port"],
+    "token": PAYLOAD_CATALOG_REGISTER["token"],
+    "service_id": PAYLOAD_AGENT_SERVICE_REGISTER["id"]
+    
+}
+
+
+
+
 
 
 def test_catalog_register():
@@ -66,24 +97,38 @@ def test_catalog_register():
     serviceCall = ServiceCall(
         domain=DOMAIN, service="catalog_register", data=PAYLOAD_CATALOG_REGISTER, context=Context())
 
-    assert consul.catalog.register.register(call=serviceCall) == True
+    assert consul.catalog.service_register(call=serviceCall) == True
 
 
 def test_catalog_deregister():
     serviceCall = ServiceCall(
         domain=DOMAIN, service="catalog_deregister", data=PAYLOAD_CATALOG_DEREGISTER, context=Context())
 
-    assert consul.catalog.deregister.deregister(call=serviceCall) == True
+    assert consul.catalog.service_deregister(call=serviceCall) == True
 
 
 def test_kv_create_or_update():
     serviceCall = ServiceCall(
         domain=DOMAIN, service="kv_create_or_update", data=PAYLOAD_KV_CREATE_OR_UPDATE, context=Context())
 
-    assert consul.kv.create_or_update.create_or_update(call=serviceCall) == True
+    assert consul.kv.create_or_update(call=serviceCall) == True
+
 
 def test_kv_delete():
     serviceCall = ServiceCall(
         domain=DOMAIN, service="kv_delete", data=PAYLOAD_KV_DELETE, context=Context())
 
-    assert consul.kv.delete.delete(call=serviceCall) == True
+    assert consul.kv.delete(call=serviceCall) == True
+
+
+def test_agent_service_register():
+    serviceCall = ServiceCall(
+        domain=DOMAIN, service="agent_service_register", data=PAYLOAD_AGENT_SERVICE_REGISTER, context=Context())
+
+    assert consul.agent.service_register(call=serviceCall) == True
+
+def test_agent_service_deregister():
+    serviceCall = ServiceCall(
+        domain=DOMAIN, service="agent_service_deregister", data=PAYLOAD_AGENT_SERVICE_DEREGISTER, context=Context())
+
+    assert consul.agent.service_deregister(call=serviceCall) == True
